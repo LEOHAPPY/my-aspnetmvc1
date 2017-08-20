e9e0636341892ad8fe2d1bd7a15aa62b53bf4095
gitLeoHappy <liuchao@my.swjtu.edu.cn>
2017-08-20 08:35:39 +0800

    Add project files.

---
+++ my-aspnetmvc1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(my_aspnetmvc1.Startup))]
+namespace my_aspnetmvc1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
