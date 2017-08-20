3e3115270297a1cc61fbd576938067c5f1a9fdc3
gitLeoHappy <liuchao@my.swjtu.edu.cn>
2017-08-20 13:03:30 +0800

    basic MVC

--- my-aspnetmvc1/App_Start/RouteConfig.cs
+++ my-aspnetmvc1/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace my_aspnetmvc1
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ my-aspnetmvc1/Controllers/MoviesController.cs
@@ -0,0 +1,29 @@
+﻿using my_aspnetmvc1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace my_aspnetmvc1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movie = new Movie()
+            {
+                Id = 1,
+                Name = "Harry Potter"
+            };
+            return View(movie);
+        }
+
+        [Route("movies/release/{year}/{month}")]
+        public ActionResult ByReleasedDate(int year, int month)
+        {
+            return Content(string.Format("this is the year of {0}, the month of {1}", year, month));
+        }
+    }
+}
